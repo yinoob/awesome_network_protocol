18d0e594249b953dd845e2cd05289d79d8ab7ffb
wysss <1243876958@qq.com>
2021-01-05 02:37:13 +0800

    a simple http server

--- http/server.js
+++ http/server.js
@@ -1,9 +1,22 @@
-const http=require('http')
+const http=require("http")
 
-createServer(function (request,response){
+http.createServer(function (request,response){
     console.log('request come',request.url)
 
     response.end('hello world')
 }).listen(8888)
 console.log('server listening on 8888')
 
+/*
+const http = require("http")
+ 
+const server = http.createServer(function(req, res) {
+  // ......
+  console.log('request come',request.url)
+
+    response.end('hello world')
+});
+ 
+server.listen(3000)
+console.log('server listening on 3000')
+*/
